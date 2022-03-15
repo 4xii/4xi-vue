841e920465a4bc6f84f65f72b2a37dfc862c7e9c
shixinzhu <975795875@qq.com>
2022-03-15 23:30:28 +0800

    feat:stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,34 @@
+import { extend } from "../shared";
 class ReactiveEffect {
     private _fn: any;
-
-    constructor(fn, public scheduler?) {
+    deps = [];
+    active = true;
+    onStop?: () => void;
+    constructor(fn, public scheduler?: Function) {
         this._fn = fn
     }
     run() {
         activeEffect = this;
-        return this._fn()
+        return this._fn();
+    }
+
+    stop() {
+        if (this.active) {
+            cleanupEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
     }
 }
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
+}
+
 const targetMap = new Map()
 export function track(target, key) {
     // target -> key -> dep
@@ -25,7 +45,9 @@ export function track(target, key) {
         depsMap.set(key, dep)
     }
 
+    if (!activeEffect) return
     dep.add(activeEffect);
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -46,7 +68,15 @@ export function effect(fn, options: any = {}) {
     // fn
     const _effect = new ReactiveEffect(fn, options.scheduler);
 
+    // extend
+    extend(_effect, options)
     _effect.run();
 
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect
+    return runner
+}
+
+export function stop(runner) {
+    runner.effect.stop()
 }
