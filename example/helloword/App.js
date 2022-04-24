f080a3cb20830e71571f403b4118f7c7cadacc0b
4xii <975795875@qq.com>
2022-04-24 11:06:05 +0800

    feat:组件事件

--- example/helloword/App.js
+++ example/helloword/App.js
@@ -1,26 +1,34 @@
-import { h } from '../../lib/guide-4xi-vue.esm.js'
+import { h } from "../../lib/guide-4xi-vue.esm.js";
 
 window.self = null;
 export const App = {
-    render() {
-        window.self = this;
-        // ui
-        return h("div", {
-            id: "root",
-            class: ["red", "hard"]
+  render() {
+    window.self = this;
+    // ui
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+        onClick() {
+          console.log("click");
         },
-            //setupSate
-            //this.$el
-            "hi," + this.msg
-            //array
-            //[h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "4xi-vue")]
-        )
-    },
+        onMousedown() {
+          console.log("mousedown");
+        },
+      },
+      //setupSate
+      //this.$el
+      "hi," + this.msg
+      //array
+      //[h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "4xi-vue")]
+    );
+  },
 
-    setup() {
-        //composition api
-        return {
-            msg: "4xi-vue-1"
-        }
-    }
-}
+  setup() {
+    //composition api
+    return {
+      msg: "4xi-vue-1",
+    };
+  },
+};
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -46,7 +46,14 @@ function mountElement(vnode: any, container: any) {
     const { props } = vnode;
     for (const key in props) {
         const val = props[key];
-        el.setAttribute(key, val)
+        // 具体的click  -》 通用
+        const isOn = (key: string) => /^on[A-Z]/.test(key)
+        if (isOn(key)) {
+            const event = key.slice(2).toLowerCase()
+            el.addEventListener(event, val)
+        } else {
+            el.setAttribute(key, val)
+        }
     }
 
     container.append(el)
